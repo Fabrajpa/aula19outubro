30f6ae7ae229e23c24052ea2de664c2c8f113f3e
tempor <tempor@L306-09.senacrjedu.local>
2016-10-19 21:08:04 -0200

    Criação da classe útil

--- Algoritmos/src/MainApp.java
+++ Algoritmos/src/MainApp.java
@@ -1,9 +1,14 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class MainApp {
 
 	public static void main(String[] args) {
 
+		Random ra = new Random();
+		
+		System.out.println(ra);
+		
 	}
 
 }
